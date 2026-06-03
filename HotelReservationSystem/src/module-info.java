5b3a97818a3691e928550ce11809a602f90844ce
Alia1409 <bouzoubaaalia947@gmail.com>
2026-06-03 03:15:56 +0200

    Update module-info.java

--- HotelReservationSystem/src/module-info.java
+++ HotelReservationSystem/src/module-info.java
@@ -2,6 +2,7 @@ module HotelReservationSystem {
 	
     requires java.desktop;
     requires java.net.http;
+    requires org.junit.jupiter.api;
     
     exports view; 
     
@@ -9,4 +10,9 @@ module HotelReservationSystem {
     exports controller;
     exports service;
     exports persistence;
+    
+    opens model to org.junit.jupiter.api;
+    opens service to org.junit.jupiter.api;
+    opens controller to org.junit.jupiter.api;
+    opens persistence to org.junit.jupiter.api;
 }
